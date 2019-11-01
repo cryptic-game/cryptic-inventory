8db8850f94fbb9b062072c42874bb05796c7a915
MaxiHuHe04 <maxiherczegh@outlook.com>
2019-11-01 16:55:23 +0100

    Improved startup structure

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 import app
 
+
+# noinspection PyUnresolvedReferences
+def load_endpoints():
+    import resources.inventory
+    import resources.shop
+
+
 if __name__ == "__main__":
-    from resources.inventory import *
-    from resources.shop import *
-
-    app.wrapper.Base.metadata.create_all(bind=wrapper.engine)
-
+    load_endpoints()
     app.m.run()
